6daee4b4afad943933bc140ef4a3b99902d5c6a9
ayushrai1235 <prathibharai64@gmail.com>
2024-12-18 00:44:10 +0530

    Loader completed

--- script.js
+++ script.js
@@ -127,3 +127,29 @@ function swiper() {
 }
 
 swiper()
+
+function loader() {
+    var tl = gsap.timeline()
+
+    tl.from("#loader h3",{
+        x:40,
+        opacity:0,
+        duration:1,
+        stagger:0.1
+    })
+    tl.to("#loader h3",{
+        opacity:0,
+        x:-20,
+        duration:1,
+        stagger:0.1
+    })
+    tl.to("#loader",{
+        opacity:0
+    })
+
+    tl.to("#loader",{
+        display:"none"
+    })
+}
+
+loader()
